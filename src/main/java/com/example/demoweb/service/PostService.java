e2ca1adc89b2f0d0a4430aad23b749b37ac98eda
Barkas074 <barkas074@yandex.ru>
2022-09-05 11:27:59 +0500

    added model and list of posts

---
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -0,0 +1,17 @@
+package com.example.demoweb.service;
+
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class PostService {
+    public ArrayList<Post> listAllPosts(){
+        ArrayList<Post> arrayList = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            arrayList.add(new Post("post " + i));
+        }
+        return arrayList;
+    }
+}
