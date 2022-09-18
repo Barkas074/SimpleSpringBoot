ee22d384e28df0895cc3496d2778664e6552d04d
Barkas074 <barkas074@yandex.ru>
2022-09-19 00:12:10 +0500

    Patch applied

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,25 +1,32 @@
 package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
 public class PostService {
-    private List<Post> posts = new ArrayList<>();
-
+    private List<Post> posts;
+    @Autowired
+    PostRepository postRepository;
     {
-        for (int i = 0; i < 3; i++) {
-            posts.add(new Post((long) posts.size(), "post " + i, new Date()));
-        }
+        posts = new ArrayList<>();
+        posts.add(new Post(0L,"пост 1", new Date()));
+        posts.add(new Post(1L,"пост 2", new Date()));
+        posts.add(new Post(2L,"пост 3", new Date()));
     }
 
-    public List<Post> listAllPosts(){
-        return posts;
+    public Iterable<Post> listAllPosts(){
+        return postRepository.findAll();
     }
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(), text, new Date()));
+        long newId = posts.size();
+        posts.add(new Post(newId, text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
